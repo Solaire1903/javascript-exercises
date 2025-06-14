0f71b3d74654202124be3f45b7058cddf4ff10fd
Solaire1903 <marvkie@gmail.com>
2025-06-14 16:09:40 +0200

    Complete Task 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+    const smaller = Math.min(start, end);
+    const larger = Math.max(start, end);
+    
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+    if (start < 0 || end < 0)  return "ERROR";
+    
+    for (let i = smaller; i <= larger; i++) {
+        sum += i;
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
