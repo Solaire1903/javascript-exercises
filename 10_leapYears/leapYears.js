9641d2f6e0218fc2ac244bc170d94709f1608cb3
Solaire1903 <marvkie@gmail.com>
2025-06-14 16:28:24 +0200

    Complete Task 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 4 !== 0) return false;
+
+    if (year % 100 !== 0) return true;
+
+    return year % 400 === 0;
 
 };
 
