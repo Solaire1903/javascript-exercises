bd139b9a46a5859b62b16dc4c052b4e967595e41
Solaire1903 <marvkie@gmail.com>
2026-09-24 18:49:14 +0200

    Solve task

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,26 @@
-const contains = function() {
+const contains = function (checkedObject, checkedValue) {
+  if (Number.isNaN(checkedValue)) return true;
   
+  if (
+    typeof checkedObject !== "object" ||
+    Array.isArray(checkedObject) ||
+    checkedObject === null
+  ) {
+    
+
+    const value = checkedObject;
+    return value === checkedValue;
+  }
+
+  let containsValue = false;
+  Object.keys(checkedObject).forEach((key) => {
+    if (contains(checkedObject[key], checkedValue)) {
+      containsValue = true;
+    }
+  });
+
+  return containsValue;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
