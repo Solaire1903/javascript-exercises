2836dbffabe8670dc6f7f1db1d14fc5dc410ef8b
Solaire1903 <marvkie@gmail.com>
2026-09-25 16:50:57 +0200

    Solve Task

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,20 @@
-const pascal = function() {
-  
+const pascal = function (n) {
+  //Base Case
+  if (n < 1) return [];
+  if (n === 1) return [1];
+
+  //Recursive Case
+  const previousRow = pascal(n - 1);
+  previousRow.unshift(0);
+  previousRow.push(0);
+
+  const currentRow = [];
+  for (let i = 0; i < n; i++) {
+    currentRow.push(previousRow[i] + previousRow[i + 1]);
+  }
+
+  return currentRow;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
